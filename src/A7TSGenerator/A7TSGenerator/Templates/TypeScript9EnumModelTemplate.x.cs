d59136bdde7251b67d3e5471ed7da603c092966c
Jonathan Harrison <jharrison@async7.com>
2014-11-03 23:25:41 -0600

    Added Enumerations as TypeScript Models and as Parameters (including defaults)

---
+++ src/A7TSGenerator/A7TSGenerator/Templates/TypeScript9EnumModelTemplate.x.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A7TSGenerator.Templates
+{
+    public partial class TypeScript9EnumModelTemplate
+    {
+
+        public TypeScript9EnumModelTemplate(Type modelType)
+        {
+            this.ModelType = modelType;
+        }
+
+        public Type ModelType { get; set; }
+        public string Properties { get { return getTypeScriptProperties(); } }
+
+        private string getTypeScriptProperties()
+        {
+            var lstFields = new List<string>();
+
+            ModelType
+                    .GetFields(BindingFlags.Public |
+                                   BindingFlags.Static)
+                    .ToList()
+                    .ForEach(fld =>
+                    {
+                        lstFields.Add(fld.Name + " = " + ((int)fld.GetValue(this)).ToString());
+                    });
+
+            return String.Join("," + Environment.NewLine + "    	", lstFields.ToArray());
+        }
+    }
+}
